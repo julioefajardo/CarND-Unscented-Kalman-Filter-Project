f2fa67a91af958bc8fae258640283331f21fa19e
julioefajardo <julioefajardo@gmail.com>
2017-05-08 10:58:00 -0600

    Initial commit

---
+++ src/tools.h
@@ -0,0 +1,30 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+#include <vector>
+#include "Eigen/Dense"
+
+class Tools {
+public:
+  /**
+  * Constructor.
+  */
+  Tools();
+
+  /**
+  * Destructor.
+  */
+  virtual ~Tools();
+
+  /**
+  * A helper method to calculate RMSE.
+  */
+  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, const std::vector<Eigen::VectorXd> &ground_truth);
+
+  /**
+  * A helper method to normalize angles between [-pi, pi].
+  */
+  double AngleNormalization(double& angle);
+
+};
+
+#endif /* TOOLS_H_ */
